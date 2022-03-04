05981f33d5ae9d3ec353c247e2cac33e65e55cd6
Andreas Casillas <m221068@usna.edu>
2022-03-04 14:09:27 -0500

    got the site to run, no cookies no cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+    {
+        name: "Paddington Bear",
+        description: "A stuff teddy bear of Paddington",
+        price: 30.00,
+        image: "paddingtonbear.jpg",
+        stock: "1"
+    },
+    {
+        name: "Goop",
+        description: "Goop! The goopiest goop you'll ever goop!",
+        price: 1000000.00,
+        image: "goop.jpg",
+        stock: "1"
+    },
+    {
+        name: "Pilsbury DoughBoy",
+        description: "An 18in styrofoam statue of the DoughBoy himself",
+        price: 10.00,
+        image: "pdb.jpg",
+        stock: "1"
+    }
+])
